f2203bc2723ea6fadf81359be693c9dc7dbaec48
AnamSadat <anamsadat3@gmail.com>
2025-10-20 17:16:12 +0700

    add README.md and delete unused file or folder

--- spring-core/spring-framework-6.2.12/spring-core/src/main/java/org/springframework/core/type/filter/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Core support package for type filtering (for example, for classpath scanning).
- */
-@NonNullApi
-@NonNullFields
-package org.springframework.core.type.filter;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
--- spring-core/spring-framework-6.2.12/spring-messaging/src/main/java/org/springframework/messaging/simp/annotation/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Annotations and for handling messages from Simple Messaging Protocols such as STOMP.
- */
-@NonNullApi
-@NonNullFields
-package org.springframework.messaging.simp.annotation;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
